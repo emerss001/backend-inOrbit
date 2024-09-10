af7e66df21c99a4670617022abb0ef527cb9c608
emerss001 <emersonnevess80@gmail.com>
2024-09-10 18:10:33 -0300

    criando funcionalidade de criação de metas

---
+++ src/features/create-goals.ts
@@ -0,0 +1,23 @@
+import { db } from "../db";
+import { goals } from "../db/schema";
+
+interface CreateGoalsRequest {
+    title: string;
+    desiredWeeklyFrequency: number;
+}
+
+export async function createGoal({ desiredWeeklyFrequency, title }: CreateGoalsRequest) {
+    const result = await db
+        .insert(goals)
+        .values({
+            title,
+            desiredWeeklyFrequency,
+        })
+        .returning();
+
+    const goal = result[0];
+
+    return {
+        goal,
+    };
+}
