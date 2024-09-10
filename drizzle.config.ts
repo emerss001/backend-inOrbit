98e1cf81574513d0735c37e3ca8d9def60105d3a
emerss001 <emersonnevess80@gmail.com>
2024-09-10 17:19:37 -0300

    criando primeira migration do banco de dados

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import { env } from "./src/env";
 
 export default defineConfig({
+    schema: "./src/db/schema.ts",
     out: "./.migrations",
     dialect: "postgresql",
     dbCredentials: {
