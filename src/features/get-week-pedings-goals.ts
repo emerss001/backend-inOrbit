10c492b2471919079198f1adaa3de2be67c67b3f
emerss001 <emersonnevess80@gmail.com>
2024-09-12 22:35:39 -0300

    adicionando função para contagem de metas concluidas nessa semana

--- src/features/get-week-pedings-goals.ts
+++ src/features/get-week-pedings-goals.ts
@@ -1,15 +1,16 @@
 import dayjs from "dayjs";
 import WeekOfYear from "dayjs/plugin/weekOfYear";
 import { db } from "../db";
-import { goals } from "../db/schema";
-import { title } from "process";
-import { lte } from "drizzle-orm";
+import { goalCompletions, goals } from "../db/schema";
+import { and, count, gte, lte } from "drizzle-orm";
 
 dayjs.extend(WeekOfYear);
 
 export function getWeekPedingsGoals() {
+    const firstDayOfWeek = dayjs().startOf("week").toDate();
     const lastDayOfWeek = dayjs().endOf("week").toDate();
 
+    // Todas as metas criadas até essa semana
     const goalsCreatedUpToWeek = db.$with("goals_created_up_to_week").as(
         db
             .select({
@@ -20,5 +21,17 @@ export function getWeekPedingsGoals() {
             })
             .from(goals)
             .where(lte(goals.createdAt, lastDayOfWeek))
+    );
+
+    // Constagem de metas concluidas nessa semana
+    const goalsCompletionCounts = db.$with("goals_completion_counts").as(
+        db
+            .select({
+                goalId: goalCompletions.goalId,
+                completionCount: count(goalCompletions.id),
+            })
+            .from(goalCompletions)
+            .where(and(gte(goals.createdAt, firstDayOfWeek), lte(goals.createdAt, lastDayOfWeek)))
+            .groupBy(goalCompletions.goalId)
     );
 }
