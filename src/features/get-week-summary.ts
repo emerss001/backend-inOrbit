20529598ed0598361c01771684629f3a0a6bb5d0
emerss001 <emersonnevess80@gmail.com>
2024-09-18 21:05:19 -0300

    organizando dados retornados

--- src/features/get-week-summary.ts
+++ src/features/get-week-summary.ts
@@ -56,7 +56,16 @@ export async function getWeekSummary() {
 
     const result = await db
         .with(goalsCreatedUpToWeek, goalsCompletedInWeek, goalsCompletedByWeekDay)
-        .select()
+        .select({
+            completed: sql/*sql*/ `(SELECT COUNT(*) FROM ${goalsCompletedInWeek})`.mapWith(Number),
+            total: sql/*sql*/ `(SELECT SUM(${goalsCreatedUpToWeek.desiredWeeklyFrequency}) FROM ${goalsCreatedUpToWeek})`.mapWith(
+                Number
+            ),
+            goalsPerDay: sql/*sql*/ `JSON_OBJECT_AGG(
+                ${goalsCompletedByWeekDay.completedAtDate},
+                ${goalsCompletedByWeekDay.completions}
+            )`,
+        })
         .from(goalsCompletedByWeekDay);
     return { sumary: result };
 }
